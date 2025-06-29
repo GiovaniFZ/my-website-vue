0aaf6e779e2e6af32dfcd73ee46934f97f643f0c
GiovaniFZ <giovanifinazzi@gmail.com>
2025-06-29 11:34:22 -0300

    feat: create simple header

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <a class="first-anchor">Giv's website</a>
+    <a>About me</a>
+    <a>My skills</a>
+    <a>Contact</a>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: #1E6448;
+  padding: 1rem;
+  border-radius: 8px;
+  display: flex;
+  gap: 0.5rem;
+  a {
+    color: #fff;
+    float: right;
+  }
+}
+
+</style>
