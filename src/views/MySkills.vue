a04303c78fbb051061e102d2477916690e97ec00
GiovaniFZ <giovanifinazzi@gmail.com>
2025-07-05 20:39:59 -0300

    conf: vue slider

--- src/views/MySkills.vue
+++ src/views/MySkills.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import RoundedSection from '../components/RoundedSection.vue';
-
 </script>
 
 <template>
     <RoundedSection :title="$t('skills')">
-        <v-icon name="co-python" scale="7" />
+        <Swiper class="mySwiper" :slides-per-view="1" :space-between="10" :navigation="true"
+            :pagination="{ clickable: true }">
+            <SwiperSlide> <v-icon name="co-python" scale="5" /></SwiperSlide>
+            <SwiperSlide> <v-icon name="co-python" scale="5" /></SwiperSlide>
+        </Swiper>
     </RoundedSection>
 </template>
