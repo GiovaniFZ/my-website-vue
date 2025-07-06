36205edad8d814f92a99e42370fdaf8ae3f985dc
GiovaniFZ <giovanifinazzi@gmail.com>
2025-07-06 13:06:02 -0300

    ref: improve slider

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { FaFlag } from "oh-vue-icons/icons/fa";
-import { RiZhihuFill } from "oh-vue-icons/icons/ri";
 import { addIcons, OhVueIcon } from "oh-vue-icons";
-import { BiLightbulbFill, BiMusicPlayer, CoPython } from "oh-vue-icons/icons";
+import { BiLightbulbFill, CoJava, CoPython, SiMongodb } from "oh-vue-icons/icons";
 import { router } from "./routes";
 import { i18n } from "./i18n";
 
-addIcons(FaFlag, RiZhihuFill, BiLightbulbFill, BiMusicPlayer, CoPython);
+addIcons(BiLightbulbFill, CoPython, CoJava, SiMongodb);
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
--- src/views/MySkills.vue
+++ src/views/MySkills.vue
@@ -1,14 +1,42 @@
 <script setup lang="ts">
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import RoundedSection from '../components/RoundedSection.vue';
 </script>
 
 <template>
     <RoundedSection :title="$t('skills')">
-        <Swiper class="mySwiper" :slides-per-view="1" :space-between="10" :navigation="true"
+        <swiper class="mySwiper" :slides-per-view="5" :space-between="2" :navigation="true"
             :pagination="{ clickable: true }">
-            <SwiperSlide> <v-icon name="co-python" scale="5" /></SwiperSlide>
-            <SwiperSlide> <v-icon name="co-python" scale="5" /></SwiperSlide>
-        </Swiper>
+            <swiper-slide>
+                <v-icon name="co-java" scale="5" />
+                <p>Java</p>
+            </swiper-slide>
+            <swiper-slide>
+                <v-icon name="co-python" scale="5" />
+                <p>Python</p>
+            </swiper-slide>
+            <swiper-slide>
+                <v-icon name="si-mongodb" scale="5" />
+                <p>MongoDB</p>
+            </swiper-slide>
+        </swiper>
     </RoundedSection>
 </template>
+
+<style>
+.mySwiper {
+    background-color: rgb(96, 168, 157);
+    border-radius: 8px;
+}
+
+.swiper-slide {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
