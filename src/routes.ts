4a5ddab94c9bd61b16764957b01c886ec6ffa74e
GiovaniFZ <giovanifinazzi@gmail.com>
2025-07-20 22:40:24 -0300

    feat: create initial files for contact screen

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import AboutMe from './views/AboutMe.vue'
 import MySkills from './views/MySkills.vue'
 import Music from './views/Music.vue'
+import Contact from './views/Contact.vue'
 
 const routes = [
     { path: '/', component: AboutMe },
     { path: '/skills', component: MySkills },
     { path: '/music', component: Music },
+    { path: '/contact', component: Contact },
 ]
 
 export const router = createRouter({
---
+++ src/views/Contact.vue
@@ -0,0 +1,5 @@
+<template>
+    <RoundedSection :title="$t('contact')" :iconName="'co-apple-music'">
+        <p>Page under construction. Come back later</p>
+    </RoundedSection>
+</template>
