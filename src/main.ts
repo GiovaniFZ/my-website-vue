32008c7510627fd437ee780827c340565c71100d
GiovaniFZ <giovanifinazzi@gmail.com>
2025-08-01 21:35:23 -0300

    feat: contact: add some icons

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import { addIcons, OhVueIcon } from "oh-vue-icons";
-import { BiLightbulbFill, CoJava, CoPython, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson } from "oh-vue-icons/icons";
+import { BiLightbulbFill, CoJava, CoPython, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson, BiTelegram, BiGithub } from "oh-vue-icons/icons";
 import { router } from "./routes";
 import { i18n } from "./i18n";
 
-addIcons(BiLightbulbFill, CoPython, CoJava, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson);
+addIcons(BiLightbulbFill, CoPython, CoJava, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson, BiTelegram, BiGithub);
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import RoundedSection from '../components/RoundedSection.vue';
-
 </script>
+
+<style scoped>
+.contact-icons {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    margin-top: 20px;
+}
+</style>
 
 <template>
     <RoundedSection :title="$t('contact')" :iconName="'io-person'">
-        <p>Page under construction. Come back later</p>
-        <p>Here you'll see my social medias!</p>
+        <div class="contact-icons">
+            <a href="https://t.me/givfnz2" target="_blank" rel="noopener noreferrer">
+                <v-icon name="bi-telegram" scale="5" />
+            </a>
+            <a href="https://github.com/GiovaniFZ" target="_blank" rel="noopener noreferrer">
+                <v-icon name="bi-github" scale="5" />
+            </a>
+        </div>
     </RoundedSection>
 </template>
