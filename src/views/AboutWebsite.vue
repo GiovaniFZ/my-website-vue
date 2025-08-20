c849d347013ca430a9ba8f6f0bf83c2b48f93c2d
GiovaniFZ <giovanifinazzi@gmail.com>
2025-08-20 19:04:01 -0300

    feat: aboutWeb: show package.version

--- src/views/AboutWebsite.vue
+++ src/views/AboutWebsite.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import RoundedSection from '../components/RoundedSection.vue';
+import Package from '../../package.json'
 
 </script>
 
 <template>
     <RoundedSection :title="$t('aboutThisWebsite')">
         <p>{{ $t('aboutWebText') }}</p>
+        <p>Version: {{ Package.version }}</p>
     </RoundedSection>
 </template>
