83b81764523ed80b122f49a912435b0aec41ad59
GiovaniFZ <giovanifinazzi@gmail.com>
2025-08-29 11:38:27 -0300

    ref: create a separated file for icons

---
+++ src/lib/icons.ts
@@ -0,0 +1,12 @@
+import { addIcons } from "oh-vue-icons";
+import {
+    BiLightbulbFill, CoJava, CoPython, CoLinux, SiMongodb, CoAndroid,
+    CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson,
+    BiTelegram, BiGithub, BiInstagram
+} from "oh-vue-icons/icons";
+
+addIcons(
+    BiLightbulbFill, CoPython, CoJava, SiMongodb, CoAndroid,
+    CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson,
+    BiTelegram, BiGithub, BiInstagram, CoLinux
+);
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import { addIcons, OhVueIcon } from "oh-vue-icons";
-import { BiLightbulbFill, CoJava, CoPython, CoLinux, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson, BiTelegram, BiGithub, BiInstagram } from "oh-vue-icons/icons";
+import "./lib/icons";
+import { OhVueIcon } from "oh-vue-icons";
 import { router } from "./routes";
 import { i18n } from "./i18n";
 
-addIcons(BiLightbulbFill, CoPython, CoJava, SiMongodb, CoAndroid, CoVueJs, CoReact, CoWindows, CoAppleMusic, IoPerson, BiTelegram, BiGithub, BiInstagram, CoLinux);
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
 app.use(router);
-app.use(i18n)
+app.use(i18n);
 app.mount("#app");
