2d3711444e81188f84e8493dd6f3bf78187f50eb
GiovaniFZ <giovanifinazzi@gmail.com>
2026-09-10 11:59:15 -0300

    feat: include neural network version. Credits: @purcas

--- src/views/AboutMe.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import RoundedSection from '../components/RoundedSection.vue';
-</script>
-
-<template>
-  <RoundedSection image="https://github.com/GiovaniFZ.png" :title="$t('greeting')" :subtitle="$t('introduction')" isFirst hasImage>
-    <p>{{ $t('aboutMeText') }}</p>
-  </RoundedSection>
-</template>
