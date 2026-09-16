ea50958c68c68f80d3312d93343fe3982fff2a6a
GiovaniFZ <giovanifinazzi@gmail.com>
2026-09-15 21:17:53 -0300

    feat: add russian and german support

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,8 @@
 import { createI18n } from "vue-i18n";
 import enUS from "./locales/en-us.json";
 import ptBR from "./locales/pt-br.json";
+import deDE from './locales/de-de.json';
+import ruRU from './locales/ru-ru.json'
 
 export const i18n = createI18n({
   locale: 'English',
@@ -8,6 +10,8 @@ export const i18n = createI18n({
   sync: true,
   messages: {
     English: enUS,
-    Português: ptBR
+    Português: ptBR,
+    Deutsch: deDE,
+    Русский: ruRU
   },
 })
