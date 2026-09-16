edcb9e4c199e597f1a9ebaab346df3e93ae13267
GiovaniFZ <giovanifinazzi@gmail.com>
2026-09-15 21:54:14 -0300

    refactor: apply icon scale on contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import RoundedSection from '../components/RoundedSection.vue';
+import { getIconScale, iconScale } from '../utils/get-icon-scale.ts';
+
+onMounted(() => {
+  getIconScale()
+  window.addEventListener('resize', getIconScale)
+})
+
 </script>
 
 <style scoped>
@@ -31,13 +39,13 @@ import RoundedSection from '../components/RoundedSection.vue';
   <RoundedSection :subtitle="$t('socialNetworks')" :title="$t('my')" :iconName="'io-person'">
     <div class="contact-icons">
       <a href="https://www.linkedin.com/in/giovani-furigo-finazzi-3b1337264/" target="_blank" rel="noopener noreferrer">
-        <v-icon name="fa-linkedin" scale="5" />
+        <v-icon name="fa-linkedin" :scale="iconScale" />
       </a>
       <a href="https://github.com/GiovaniFZ" target="_blank" rel="noopener noreferrer">
-        <v-icon name="bi-github" scale="5" />
+        <v-icon name="bi-github" :scale="iconScale" />
       </a>
       <a href="https://instagram.com/giovani.finazzi" target="_blank" rel="noopener noreferrer">
-        <v-icon name="bi-instagram" scale="5" />
+        <v-icon name="bi-instagram" :scale="iconScale" />
       </a>
     </div>
   </RoundedSection>
