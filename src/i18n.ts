494c5b1915443d1d128d32327f83d4209a6b8c9b
GiovaniFZ <giovanifinazzi@gmail.com>
2026-09-15 21:25:55 -0300

    feat: get the system language automatically

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,11 +2,24 @@ import { createI18n } from "vue-i18n";
 import enUS from "./locales/en-us.json";
 import ptBR from "./locales/pt-br.json";
 import deDE from './locales/de-de.json';
-import ruRU from './locales/ru-ru.json'
+import ruRU from './locales/ru-ru.json';
+
+function getSystemLanguage() {
+  const languageMap: Record<string,string> = {
+    pt: 'Português',
+    de: 'Deutsch',
+    ru: 'Русский'
+  };
+
+  const browserLang = navigator.language || 'en';
+  const shortLang = browserLang.substring(0, 2);
+
+  return languageMap[shortLang] || 'English';
+}
 
 export const i18n = createI18n({
-  locale: 'English',
-  fallbackLocale: 'Português',
+  locale: getSystemLanguage(),
+  fallbackLocale: 'English',
   sync: true,
   messages: {
     English: enUS,
@@ -14,4 +27,4 @@ export const i18n = createI18n({
     Deutsch: deDE,
     Русский: ruRU
   },
-})
+});
